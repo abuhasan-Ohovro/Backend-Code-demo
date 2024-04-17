b5b2d64fcba3790e21978cc3bae05ea7903840c4
abuhasan-Ohovro <abuhasanmondal24@gmail.com>
2024-04-17 22:26:13 +0600

    bassic express app done

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is ready");
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+});
+app.get('/jokes',(req, res) => {
+    const joke = [
+        {
+            id: 1,
+            title: 'A joke',
+            constent: 'This is a joke'
+
+        },
+        {
+            id: 2,
+            title: 'A joke',
+            constent: 'This is a second joke'
+        },
+        {
+            id: 2,
+            title: 'A joke',
+            constent: 'This is a second joke'
+        },
+        {
+            id: 3,
+            title: 'A joke',
+            constent: 'This is a third joke'
+        },
+        {
+            id: 4,
+            title: 'A joke',
+            constent: 'This is a forth  joke'
+        },
+    ];
+    res.send(joke);
+});
